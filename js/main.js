fb0630d7c38efd3c1b8928b19a5a2234922b3762
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-31 23:03:45 -0400

    add spacing between lines to make code easier to read

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function () {
+
     $('img').remove();
 
     var headings = document.getElementsByTag('h2');
 
     headings.last().text("I'm JMF Hernandez, and I'm learning jQuery!");
+
 });
