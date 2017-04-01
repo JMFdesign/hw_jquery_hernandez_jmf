70ebba49d207d9eb169469e662bb31109c6f029a
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-31 22:54:24 -0400

    add an 's' to 'Element' in 'getElementByTag'

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $('img').remove();
 
-    var headings = document.getElementByTag('h2');
+    var headings = document.getElementsByTag('h2');
 
     headings.last().text("I'm JMF Hernandez, and I'm learning jQuery!");
 });
