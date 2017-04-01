435bc32cd99a931ef80f6109b41b0b2be9981fa3
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-04-01 00:17:33 -0400

    revise assignment of <h2> to var 'headings'

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     $('img').remove();
 
-    var headings = document.getElementsByTagName('h2');
+    var headings = $('h2');
 
     headings.last().text("I'm JMF Hernandez, and I'm learning jQuery!");
 
