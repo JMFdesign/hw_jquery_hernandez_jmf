beb35e5e3089298fd7b59449120f699dac246158
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-03-31 22:51:13 -0400

    add back the jQuery to remove the images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function () {
+    $('img').remove();
+
     var headings = document.getElementByTag('h2');
 
     headings.last().text("I'm JMF Hernandez, and I'm learning jQuery!");
